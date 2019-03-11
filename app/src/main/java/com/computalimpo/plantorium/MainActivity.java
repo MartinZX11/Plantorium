2edf9989fbe4910679fdbe543e27cb9950abf7ba
Martin <martinzx11@gmail.com>
2019-03-11 17:06:44 +0100

    Link fragments to navigationBar

--- app/src/main/java/com/computalimpo/plantorium/MainActivity.java
+++ app/src/main/java/com/computalimpo/plantorium/MainActivity.java
@@ -2,9 +2,16 @@ package com.computalimpo.plantorium;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+
+import com.computalimpo.plantorium.fragments.CropsFragment;
+import com.computalimpo.plantorium.fragments.MapFragment;
+import com.computalimpo.plantorium.fragments.QRFragment;
+import com.computalimpo.plantorium.fragments.TaskFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -12,10 +19,50 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crops);
+        ((BottomNavigationView) findViewById(R.id.bottomNavigationView)).setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+        Fragment fragment = null;
+        String tag = null;
+        switch (menuItem.getItemId()){
+            case R.id.crops:
+                tag = "crops";
+                Log.d("tag1", "onNavigationItemSelected: ");
+                fragment= getSupportFragmentManager().findFragmentByTag(tag);
+                if(fragment== null) {
+                    fragment=  new CropsFragment();
+                }getSupportActionBar().setTitle(R.string.crops);
+                break;
+            case R.id.map:
+                tag = "map";
+                Log.d("tag1", "onNavigationItemSelected: ");
+                fragment= getSupportFragmentManager().findFragmentByTag(tag);
+                if(fragment== null) {
+                    fragment=  new MapFragment();
+                }getSupportActionBar().setTitle(R.string.map);
+                break;
+            case R.id.qr:
+                tag = "qr";
+                Log.d("tag1", "onNavigationItemSelected: ");
+                fragment= getSupportFragmentManager().findFragmentByTag(tag);
+                if(fragment== null) {
+                    fragment=  new QRFragment();
+                }getSupportActionBar().setTitle(R.string.qr);
+                break;
+            case R.id.task:
+                tag = "task";
+                Log.d("tag1", "onNavigationItemSelected: ");
+                fragment= getSupportFragmentManager().findFragmentByTag(tag);
+                if(fragment== null) {
+                    fragment=  new TaskFragment();
+                }getSupportActionBar().setTitle(R.string.task);
+                break;
+
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment, tag).commit();
+        return true;
+
     }
 }
