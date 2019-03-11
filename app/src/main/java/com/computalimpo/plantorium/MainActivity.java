6b13ef2b60ab415109c330f135abb6806b8f0796
Martin <martinzx11@gmail.com>
2019-03-11 16:32:09 +0100

    Added navigationBottomView

---
+++ app/src/main/java/com/computalimpo/plantorium/MainActivity.java
@@ -0,0 +1,21 @@
+package com.computalimpo.plantorium;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crops);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+}
