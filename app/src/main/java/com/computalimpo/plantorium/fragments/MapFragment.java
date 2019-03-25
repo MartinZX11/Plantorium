2f0582de55969ea99518bdf5d1ce90b40d9e8538
Raúl <raucre44@gmail.com>
2019-03-25 17:14:41 +0100

    Markers para el mapa

--- app/src/main/java/com/computalimpo/plantorium/fragments/MapFragment.java
+++ app/src/main/java/com/computalimpo/plantorium/fragments/MapFragment.java
@@ -10,33 +10,48 @@ import android.view.ViewGroup;
 
 import com.computalimpo.plantorium.R;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements com.google.android.gms.maps.OnMapReadyCallback{
 
-    private MapView mapView;
+
     private GoogleMap googleMap;
+    public SupportMapFragment mapFragment;
 
     public MapFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View mapView = inflater.inflate(R.layout.fragment_map, null);
 
-        return inflater.inflate(R.layout.fragment_map, null);
+        return mapView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mapView = (MapView) view.findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);
-        mapView.onResume();
-        mapView.getMapAsync(this);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
         googleMap = map;
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                MarkerOptions options = new MarkerOptions();
+                options.position(point);
+                options.icon(BitmapDescriptorFactory.fromResource(R.drawable.tree_icon));
+                googleMap.addMarker(options);
+            }
+        });
     }
+
+
 }
