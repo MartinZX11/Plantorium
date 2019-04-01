595ca70ee3134f6ba882a6427bc1365b2951bba2
raugarcr <raucre44@gmail.com>
2019-04-01 17:53:04 +0200

    generar QR

--- app/src/main/java/com/computalimpo/plantorium/fragments/QRFragment.java
+++ app/src/main/java/com/computalimpo/plantorium/fragments/QRFragment.java
@@ -3,6 +3,8 @@ package com.computalimpo.plantorium.fragments;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -11,10 +13,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.computalimpo.plantorium.R;
 import com.computalimpo.plantorium.Scaner;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QRFragment extends Fragment {
 
@@ -23,8 +31,10 @@ public class QRFragment extends Fragment {
     private static final int CAMERA_PERMISSION = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View result =inflater.inflate(R.layout.fragment_qr, null);
-        Button scan = result.findViewById(R.id.scanear);
+        final View qrFragment =inflater.inflate(R.layout.fragment_qr, null);
+        Button scan = qrFragment.findViewById(R.id.scanear);
+        Button gen = qrFragment.findViewById(R.id.generar);
+
 
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +52,34 @@ public class QRFragment extends Fragment {
 
             }
         });
-        return result;
+
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imagen = qrFragment.findViewById(R.id.imagenQR);
+                EditText entrada = qrFragment.findViewById(R.id.entrada);
+                QRCodeWriter writer = new QRCodeWriter();
+                try {
+                    BitMatrix bitMatrix = writer.encode(entrada.getText().toString(), BarcodeFormat.QR_CODE, 512, 512);
+                    int width = bitMatrix.getWidth();
+                    int height = bitMatrix.getHeight();
+                    Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                    for (int x = 0; x < width; x++) {
+                        for (int y = 0; y < height; y++) {
+                            bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                        }
+                    }
+                    imagen.setImageBitmap(bmp);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+        return qrFragment;
     }
 
     @Override
@@ -50,7 +87,9 @@ public class QRFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case INTENT_CODE:
-                Toast.makeText(getActivity(), data.getStringExtra("result"), Toast.LENGTH_SHORT).show();
+
+                    Toast.makeText(getActivity(), data.getStringExtra("result"), Toast.LENGTH_SHORT).show();
+
                 break;
         }
     }
