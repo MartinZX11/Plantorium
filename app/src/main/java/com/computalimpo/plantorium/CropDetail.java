aa45722591419d11d8b24f7f91ce56f343f420ba
Raúl <raucre44@gmail.com>
2019-04-09 16:31:19 +0200

    Changes some strings in the crop fragment and added save file image

--- app/src/main/java/com/computalimpo/plantorium/CropDetail.java
+++ app/src/main/java/com/computalimpo/plantorium/CropDetail.java
@@ -1,5 +1,7 @@
 package com.computalimpo.plantorium;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -14,6 +16,10 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CropDetail extends AppCompatActivity {
 
@@ -51,9 +57,38 @@ public class CropDetail extends AppCompatActivity {
                 }
             }
             qrImagen.setImageBitmap(bmp);
+            //saveToInternalStorage(bmp);
 
         } catch (WriterException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+
+
+    private String saveToInternalStorage (Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 }
